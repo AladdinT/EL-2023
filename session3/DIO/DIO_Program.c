589299a033fa007ef8b64fdc4c7abc0cd8527ed1
AladdinT <tuhami.10.8@gmail.com>
2023-08-29 17:39:57 +0300

    mv

--- session3/DIO/DIO_Program.c
+++
@@ -1,4 +0,0 @@
-void Init_PORTA_DIR (void)
-{
-	DDRA = 11110000;
-}
--- session3/s3_task4_header_exel/DIO.h
+++
@@ -1,8 +0,0 @@
-void DIO_SetPinValue(u8 Port ,u8 Pin , u8 value);
-void DIO_SetPinDirection(u8 Port, u8 Pin, u8 Direct);
-void DIO_SetPortValue(u8 Port, u8 value);
-void DIO_TogglePinValue(u8 Port ,u8 Pin);
-u8 DIO_GetPinValue(u8 Port,u8 Pin);
-u8 DIO_GetPortValue(u8 Port);
-
-void DIO_SetPortDirection(u8 Direct , u8 value);
